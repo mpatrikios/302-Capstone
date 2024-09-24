51e7690a2244775b7bdb930f023c46c93d84aa4f
Prometheus41503 <jdjb@outlook.com>
2024-09-24 16:12:03 -0400

    Initial Commit

---
+++ Forager/ForagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForagerApp.swift
+//  Forager
+//
+//  Created by Jakob Dallas Johnson Brown on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ForagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForagerTests/ForagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForagerTests.swift
+//  ForagerTests
+//
+//  Created by Jakob Dallas Johnson Brown on 9/24/24.
+//
+
+import Testing
+@testable import Forager
+
+struct ForagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
